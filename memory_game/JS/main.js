d707e1c65bd24aab8a24e9e5d1633cd27b357aae
Andre Urbani <andreurbani@Andres-MacBook-Pro.local>
2019-09-24 11:29:32 +0100

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -4,24 +4,27 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-alert("You found a match!");
-} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-alert("Sorry try again!");
+function checkFormatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
 }
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]); 
+
+cardsInPlay.push(cards[cardId]);
+
+}
 
 
 
-console.log();
+flipCard(0);
+flipCard(2);
+
+checkFormatch();
+
+
 
