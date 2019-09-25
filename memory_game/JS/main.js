348d0a8819c4085808c951e7922bbd754a84c7ce
Andre Urbani <andreurbani@Andres-MacBook-Pro.local>
2019-09-25 14:40:30 +0100

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,30 +1,58 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	}
+];
 
 var cardsInPlay = [];
 
 function checkFormatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
+  alert("You found a match!");
 } else {
   console.log("Sorry, try again.");
+  alert("Sorry, try again!");
 }
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]); 
+	console.log("User flipped " + cards[cardId].rank); 
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
 
 }
 
 
 
 flipCard(0);
-flipCard(2);
+flipCard(1);
 
 checkFormatch();
+
+
 
 
 
